c75aba02613e050ab16be1e40009d2bff943c352
tanhHeng <2196226582@qq.com>
2024-08-31 22:55:37 +0800

    Fix: 修复部分情况下config类型错误的问题

--- where2go/config.py
+++ where2go/config.py
@@ -8,15 +8,15 @@ class COMMAND_CONFIG(Serializable):
     fastsearch_prompt: str = "XXX在哪"
 
 class PLAYER_API_CONFIG(Serializable):
-    prefix_regex: str = "(?:\[.+\])?",
-    player_pos_command: str = "data get entity {player} Pos",
-    player_dimension_command: str = "data get entity {player} Dimension",
-    player_pos_regex: str =  "^{prefix_regex}(\w+) has the following entity data: \[(-?[0-9.]+)d, (-?[0-9.]+)d, (-?[0-9.]+)d\]",
-    player_dimension_regex: str =  '^{prefix_regex}(\w+) has the following entity data: "minecraft:(\w+)"',
-    player_notfound_regex: str =  '^No entity was found',
-    player_list_command: str =  "list",
-    player_list_sep: str =  ", ",
-    player_list_regex: str =  '^There are [0-9]+ of a max of [0-9]+ players online: (.+)*'
+    prefix_regex: str = "(?:\[.+\])?"
+    player_pos_command: str = "data get entity {player} Pos"
+    player_dimension_command: str = "data get entity {player} Dimension"
+    player_pos_regex: str = "^{prefix_regex}(\w+) has the following entity data: \[(-?[0-9.]+)d, (-?[0-9.]+)d, (-?[0-9.]+)d\]"
+    player_dimension_regex: str = '^{prefix_regex}(\w+) has the following entity data: "minecraft:(\w+)"'
+    player_notfound_regex: str = '^No entity was found'
+    player_list_command: str = "list"
+    player_list_sep: str = ", "
+    player_list_regex: str = '^There are [0-9]+ of a max of [0-9]+ players online: (.+)*'
     highlight_command: str = "effect give {player} minecraft:glowing 15 0 true"
 
 class CONFIG(Serializable):
